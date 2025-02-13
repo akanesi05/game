eedf8188d34975c60964ba0b3520e572f11cf1be
mitsuiakane <akane.sumitomo@gmail.com>
2025-02-13 23:58:09 +0900

    クイズ例文

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-const quiz = [
-    {
-      question: 'ゲーム史上、最も売れたゲーム機はどれ？',
-      answers: [ 'スーパーファミコン', 'PlayStation 2', 'ニンテンドーDS', 'Xbox 360'],
-      correct: 'ニンテンドーDS'
-    }
-  ];
+const question = 'ゲーム史上、最も売れたゲーム機はどれ？';
+const answers =[ 'スーパーファミコン', 'PlayStation 2', 'ニンテンドーDS', 'Xbox 360'];
+const correct ='ニンテンドーDS';
+
+console.log(document.getElementById('js-question'));
